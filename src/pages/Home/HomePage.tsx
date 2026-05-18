e5881b395b4db2e0eb4a37f5f2ce04ab64c6e54f
unknown <tp085028@mail.apu.edu.my>
2026-05-18 14:25:35 +0700

    feat: integrate services and trust sections into homepage

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,10 +1,15 @@
 import { Hero } from '../../components/sections/Hero'
+import { Services } from '../../components/sections/Services'
+import { TrustSection } from '../../components/sections/TrustSection'
+import '../../styles/sections.css'
 
 export function HomePage() {
   return (
     <>
       <Hero />
-      {/* other sections stay unchanged for now */}
+      <Services />
+      <TrustSection />
+      {/* keep testimonials/contact unchanged for now */}
     </>
   )
 }
